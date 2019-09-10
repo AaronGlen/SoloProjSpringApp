20cace3327efe4062c60c7b74c930862d2e968b1
= <=>
2019-09-10 16:36:13 +0100

    added TeamControllerTest added testGetAllTeams

---
+++ src/test/java/com/qa/Controllers/TeamControllerTest.java
@@ -0,0 +1,56 @@
+package com.qa.Controllers;
+
+
+import com.qa.controllers.TeamController;
+import com.qa.model.Team;
+import com.qa.repository.TeamRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+public class TeamControllerTest {
+
+    @InjectMocks
+    private TeamController teamController;
+
+    @Mock
+    private TeamRepository repository;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testGetAllTeams(){
+        List<Team> teamsList = new ArrayList<>();
+        Team team = new Team();
+        team.setDescription("blah");
+        team.setTeamName("A");
+        team.setIssueOne("#1");
+        teamsList.add(team);
+
+        when(repository.findAll()).thenReturn(teamsList);
+
+        assertEquals(teamController.listAllTeams().get(0).getTeamName(), "A");
+    }
+
+
+
+}
