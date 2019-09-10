f020744920e6c73f436b23fa4e1a96f27cbac604
= <=>
2019-09-10 13:02:23 +0100

    changed application.properties so that it gets the data from mysql

--- src/main/java/com/qa/repository/HeroRepository.java
+++ src/main/java/com/qa/repository/HeroRepository.java
@@ -1,6 +1,7 @@
 package com.qa.repository;
 
 import com.qa.model.Hero;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HeroRepository extends JpaRepository<Hero, Long> {
